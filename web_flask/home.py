32fcb50973572c5bc2c1cde5f4d68bc7036bff8c
GunterPearson <1715@holbertonschool.com>
2022-03-30 15:37:02 -0500

    pushing updated home.py and updated index.html with Google analytics

--- web_flask/home.py
+++ web_flask/home.py
@@ -2,11 +2,14 @@
 """ Starts a Flask Web Application """
 from flask import Flask, render_template, session, request, jsonify
 from flask import redirect, flash, url_for
+import os
 from flask_simple_geoip import SimpleGeoIP
 from flask_cors import CORS
 app = Flask(__name__)
 cors = CORS(app, resources={r"": {"origins": "*"}})
 
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
 app.config["GEOIPIFY_API_KEY"] = "at_Odn3yiaerwjP1EPcvdotCZyrNitvL"
 
 simple_geoip = SimpleGeoIP(app)
@@ -18,8 +21,7 @@ def home():
     geoip_data = simple_geoip.get_geoip_data()
     # print(type(geoip_data))
     location = geoip_data["location"]
-    location["IP"] = request.remote_addr
     location["ip_address"] = geoip_data.get("ip")
-    return location
-    # session["user"] = request.remote_addr
-    # return render_template("index.html")
+    session["user"] = location
+    # return location
+    return render_template("index.html")
