39649a6772da70fe1f463767a6650abb783b0c3d
GunterPearson <1715@holbertonschool.com>
2022-03-30 19:06:10 -0500

    pushing updated files for sending emails with flask mail to me from personal website

--- web_flask/home.py
+++ web_flask/home.py
@@ -3,14 +3,36 @@
 from flask import Flask, render_template, session, request, jsonify
 from flask import redirect, flash, url_for
 import os
+from web_flask.forms import EmailForm
 from flask_simple_geoip import SimpleGeoIP
+from flask_mail import Message, Mail
 from flask_cors import CORS
 app = Flask(__name__)
 cors = CORS(app, resources={r"": {"origins": "*"}})
 
 SECRET_KEY = os.urandom(32)
+
+MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+LOGIN_USERNAME = os.getenv('LOGIN_USERNAME')
+LOGIN_PASSWORD = os.getenv('LOGIN_PASSWORD')
+
 app.config['SECRET_KEY'] = SECRET_KEY
 app.config["GEOIPIFY_API_KEY"] = "at_Odn3yiaerwjP1EPcvdotCZyrNitvL"
+# app.config["DEBUG"] = True
+# app.config["TESTING"] = False
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_DEBUG'] = True
+app.config['MAIL_USERNAME'] = MAIL_USERNAME
+app.config['MAIL_PASSWORD'] = MAIL_PASSWORD
+app.config['MAIL_DEFAULT_SENDER'] = None
+app.config['MAIL_MAX_SEND'] = None
+app.config['MAIL_SUPPRESS_SEND'] = False
+app.config['MAIL_ASCII_ATTATCHMENTS'] = False
+mail = Mail(app)
 
 simple_geoip = SimpleGeoIP(app)
 
@@ -18,10 +40,22 @@ simple_geoip = SimpleGeoIP(app)
 @app.route('/', strict_slashes=False, methods=['GET', 'POST'])
 def home():
     """home route for html file"""
+    form = EmailForm()
     geoip_data = simple_geoip.get_geoip_data()
     # print(type(geoip_data))
     location = geoip_data["location"]
     location["ip_address"] = geoip_data.get("ip")
     session["user"] = location
+    if request.method == "POST":
+        msg = Message(form.subject.data, sender=form.email.data, recipients=['vikepi3161@vsooc.com'])
+        msg.body = form.name.data + "\n" + form.message.data + "\n\n" + form.email.data
+        mail.send(msg)
     # return location
-    return render_template("index.html")
+    return render_template("index.html", form=form)
+
+# @app.route('/mail', strict_slashes=False, methods=['GET', 'POST'])
+# def mail():
+#     """Email route for sending me emails"""
+#     form = ContactForm()
+#     if request.method == "POST":
+        
--- web_flask/static/vendor/php-email-form/validate.js
+++ web_flask/static/vendor/php-email-form/validate.js
@@ -55,31 +55,40 @@
       body: formData,
       headers: {'X-Requested-With': 'XMLHttpRequest'}
     })
-    .then(response => {
-      if( response.ok ) {
-        return response.text()
-      } else {
-        throw new Error(`${response.status} ${response.statusText} ${response.url}`); 
-      }
-    })
+    // .then(response => {
+      
+    //   if( response.ok ) {
+    //     return response.text()
+    //   } else {
+    //     throw new Error(`${response.status} ${response.statusText} ${response.url}`); 
+    //   }
+    // })
     .then(data => {
       thisForm.querySelector('.loading').classList.remove('d-block');
-      if (data.trim() == 'OK') {
-        thisForm.querySelector('.sent-message').classList.add('d-block');
-        thisForm.reset(); 
-      } else {
-        throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
-      }
-    })
-    .catch((error) => {
-      displayError(thisForm, error);
+      // console.log(data.trim());
+      // if (data.trim() == 'OK') {
+      thisForm.querySelector('.sent-message').classList.add('d-block');
+      // document.getElementById("formula1").reset()
+      thisForm.reset();
+      // } 
+    //   else {
+    //     console.log("Inside the else");
+    //     throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
+    //   }
+    // })
+    // .catch((error) => {
+    //   console.log("Inside the Catch");
+    //   displayError(thisForm, error);
     });
   }
 
   function displayError(thisForm, error) {
     thisForm.querySelector('.loading').classList.remove('d-block');
-    thisForm.querySelector('.error-message').innerHTML = error;
-    thisForm.querySelector('.error-message').classList.add('d-block');
+    thisForm.querySelector('.sent-message').classList.add('d-block');
+    thisForm.reset(); 
+    // thisForm.querySelector('.loading').classList.remove('d-block');
+    // thisForm.querySelector('.error-message').innerHTML = error;
+    // thisForm.querySelector('.error-message').classList.add('d-block');
   }
 
 })();
